da2c6978e772cf4ee8fb7b6a0e1d600d7ed56cb0
angr1252 <47996387+angr1252@users.noreply.github.com>
2019-05-04 17:36:12 -0600

    Update script.js

--- static/script.js
+++ static/script.js
@@ -6,17 +6,18 @@ let words = [];
 let data = [];
 let label = [];
 var datasets = [];
+var pallete = ["#00876c", "#3e9669", "#65a465", "#8bb162", "#b2bd62", "#dac767", "#deae53", "#e19448", "#e17945", "#dd5c48"];
+
 
 const fetchRecent = async (minutes = 1) => {
     const res = await fetch(`https://big-data-energy.appspot.com/api/emojis?top=10&minutes=${minutes}`)
-        //const res = await fetch(`http://localhost:3000/api/emojis?top=10&minutes=1`)
         .catch(reason => console.log);
     const data = await res.json()
         .catch(reason => console.log);
     return data;
 }
-const getDataAllTime = async () => {
-    const res = await fetch(`https://big-data-energy.appspot.com/api/emojis?top=10`)
+const fetchAllTime = async () => {
+    const res = await fetch(`https://big-data-energy.appspot.com/api/emojis?top=30`)
         .catch(reason => console.log);
     const data = await res.json()
         .catch(reason => console.log);
@@ -27,8 +28,9 @@ const fetchTrend = async () => {
         .catch(reason => console.log);
     const data = await res.json()
         .catch(reason => console.log);
-    const formatted = data.map(value => ({
+    const formatted = data.map((value, i) => ({
         label: String.fromCodePoint(parseInt(value.HTML_HEX_CODE.replace('&#', '0'))),
+        backgroundColor: pallete[i],
         data: [
             value["6 DAYS AGO"],
             value["5 DAYS AGO"],
@@ -38,24 +40,8 @@ const fetchTrend = async () => {
             value["1 DAY AGO"],
         ]
     }));
-    console.log({ formatted });
     return formatted;
 }
-
-//const parseTrend = (data) => {
-//    datasets = data.map(val => ({
-//        label: value.HTML_HEX_CODE,
-//        data: [
-//            value["6 DAYS AGO"],
-//            value["5 DAYS AGO"],
-//            value["4 DAYS AGO"],
-//            value["3 DAYS AGO"],
-//            value["2 DAYS AGO"],
-//            value["1 DAY AGO"]
-//        ]
-//    }));
-//}
-
 
 let barChart = new Chart(barChartEl, {
     type: 'horizontalBar',
@@ -64,7 +50,7 @@ let barChart = new Chart(barChartEl, {
         datasets: [{
             label: "Emoji Count",
             data: [],
-            backgroundColor: ["#00876c", "#3e9669", "#65a465", "#8bb162", "#b2bd62", "#dac767", "#deae53", "#e19448", "#e17945", "#dd5c48"],
+            backgroundColor: pallete,
             borderWidth: 3
     }]
     },
@@ -87,23 +73,18 @@ let barChart = new Chart(barChartEl, {
         }
     }
 });
+const updateBarChart = (data) => {
+    const labels = data.map(val => String.fromCodePoint(parseInt(val.HTML_HEX_CODE.replace('&#', '0'))));
+    const values = data.map(val => val.TWEET_COUNT);
+    barChart.data.datasets[0].data = values;
+    barChart.data.labels = labels;
+    barChart.update();
+}
 
-//var lineData = {
-//  labels: ["last day","1 day ago", "2 days ago", "3 days ago", "4 days ago", "5 days ago", "6 days ago"],
-//  datasets: [{
-//      label: "",
-//      data: [65, 59, 80, 81, 56, 55, 40,30,60,55,30,78],
-//    }, {
-//      label: "",
-//      data: [10, 20, 60, 95, 64, 78, 90,40,70,40,70,89],
-//    }
-//
-//  ]
-//};
 let lineChart = new Chart(lineChartEl, {
-    type: 'line',
-    labels: ["1 day ago", "2 days ago", "3 days ago", "4 days ago", "5 days ago", "6 days ago"],
+    type: 'bar',
     data: {
+        labels: ["6 days ago", "5 days ago", "4 days ago", "3 days ago", "2 days ago", "1 day ago"],
         datasets: []
     },
     options: {
@@ -114,8 +95,8 @@ let lineChart = new Chart(lineChartEl, {
     }
 });
 
-/* Word cloud function */
-const updateCloud = (data) => {
+const updateCloud = async () => {
+    const data = await fetchAllTime();
     const words = data.map((val) => {
         return {
             text: String.fromCodePoint(parseInt(val.HTML_HEX_CODE.replace('&#', '0'))),
@@ -127,11 +108,12 @@ const updateCloud = (data) => {
             {
                 "type": "wordcloud",
                 "options": {
+                    maxFontSize: 100,
+                    minFontSize: 30,
                     aspect: 'spiral',
                     "style": {
                         backgroundColor: 'black',
-                        borderRadius: 2,
-                        padding: '50em 50em',
+                        borderRadius: 10,
                         margin: '60em 60em',
                         hoverState: {
                             alpha: 1,
@@ -166,30 +148,20 @@ const updateCloud = (data) => {
     });
 }
 
-const updateBarChart = (data) => {
-    const labels = data.map(val => String.fromCodePoint(parseInt(val.HTML_HEX_CODE.replace('&#', '0'))));
-    const values = data.map(val => val.TWEET_COUNT);
-    barChart.data.datasets[0].data = values;
-    barChart.data.labels = labels;
-    barChart.update();
-}
 
-// setInterval(async () => {
-//    const data = await fetchRecent();
-//    updateBarChart(data);
-//    updateCloud(data);
-// }, 10000);
+updateCloud();
 
-// setInterval(async () => {
-//    const alldata = await fetchAllTime();
-//    updateCloud(alldata);
-// }, 30000);
+ setInterval(async () => {
+    const data = await fetchRecent();
+    updateBarChart(data);
+ }, 1000);
 
 const setLineChart = async () => {
-   const trend = await fetchTrend();
-   lineChart.data.datasets = trend;
-   lineChart.update();
+    const trend = await fetchTrend();
+    lineChart.data.datasets = trend;
+    lineChart.update();
 }
+
 setLineChart();
 setInterval(async () => {
     setLineChart();
